d3614c39202fa0c1b839b061bb4d2dbca6723a82
MatheusOBarbosa <d2021029612@unifei.edu.br>
2023-06-29 20:36:57 -0300

    mudança da mensagem do servidor

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.connect(function (err) {
 
 app.get("/", (req, res) => {
     console.log("Response ok.");
-    res.send("Ok – Servidor disponível.");
+    res.send("Ok – Servidor agora está disponível.");
 });
 
 app.get("/usuarios", (req, res) => {
